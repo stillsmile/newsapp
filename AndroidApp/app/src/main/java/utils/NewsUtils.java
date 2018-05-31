5d6a5a65d2c2fa71c39efbad6926b0742b0c99d3
stillsmile <970641430@qq.com>
2018-05-31 12:43:57 +0800

    newListView测试数据

--- AndroidApp/app/src/main/java/utils/NewsUtils.java
+++ AndroidApp/app/src/main/java/utils/NewsUtils.java
@@ -20,12 +20,63 @@ import dao.NewsDaoUtils;
 public class NewsUtils {
 
     public static String newsPath_url = "http://47.98.145.175/itheima74/servlet/GetNewsServlet";
+    public static String newsPath_urlTest = "http://47.98.145.175/itheima74/servlet/GetNewsServletTest";
     public static ArrayList<NewsBean> getAllNewsForNetWork(Context context) {
         ArrayList<NewsBean> arrayList = new ArrayList<NewsBean>();
         try{
             //1.请求服务器获取新闻数据
             //获取一个url对象，通过url对象得到一个urlconnnection对象
             URL url = new URL(newsPath_url);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            //设置连接的方式和超时时间
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(10*1000);
+            //获取请求响应码
+            int code = connection.getResponseCode();
+            if(code == 200){
+                //获取请求到的流信息
+                InputStream inputStream = connection.getInputStream();
+                String result = StreamUtils.streamToString(inputStream);
+
+                //2.解析获取的新闻数据到List集合中。
+
+                JSONObject root_json = new JSONObject(result);//将一个字符串封装成一个json对象。
+                JSONArray jsonArray = root_json.getJSONArray("newss");//获取root_json中的newss作为jsonArray对象
+
+                for (int i = 0 ;i < jsonArray.length();i++){//循环遍历jsonArray
+                    JSONObject news_json = jsonArray.getJSONObject(i);//获取一条新闻的json
+
+                    NewsBean newsBean = new NewsBean();
+
+                    newsBean. id = news_json.getInt("id");
+                    newsBean. comment = news_json.getInt("comment");//评论数
+                    newsBean. type = news_json.getInt("type");//新闻的类型，0 ：头条 1 ：娱乐 2.体育
+                    newsBean. time = news_json.getString("time");
+                    newsBean. des = news_json.getString("des");
+                    newsBean. title = news_json.getString("title");
+                    newsBean. news_url = news_json.getString("news_url");
+                    newsBean. icon_url = news_json.getString("icon_url");
+
+                    arrayList.add(newsBean);
+
+                }
+
+                //3.清楚数据库中旧的数据，将新的数据缓存到数据库中
+                new NewsDaoUtils(context).delete();
+                new NewsDaoUtils(context).saveNews(arrayList);
+            }
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+    public static ArrayList<NewsBean> getAllNewsForNetWorkTest(Context context) {
+        ArrayList<NewsBean> arrayList = new ArrayList<NewsBean>();
+        try{
+            //1.请求服务器获取新闻数据
+            //获取一个url对象，通过url对象得到一个urlconnnection对象
+            URL url = new URL(newsPath_urlTest);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             //设置连接的方式和超时时间
             connection.setRequestMethod("GET");
